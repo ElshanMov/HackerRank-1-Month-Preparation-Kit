610e1591bd3e4f7fc0207ee1821995e2bfa8a10a
ElshanMov <elshan.mov.772@gmail.com>
2023-03-05 23:11:06 -0800

    PlusMinus

---
+++ PlusMinus/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PlusMinus
+{
+    internal class Program
+    {
+        static void PlusMinus(List<int> arr)
+        {
+            double positive = 0;
+            double negative = 0;
+            double zero = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positive += 1;
+                }
+                else if (arr[i] == 0)
+                {
+                    zero += 1;
+                }
+                else
+                {
+                    negative += 1;
+                }
+            }
+            Console.WriteLine(positive / arr.Count);
+            Console.WriteLine(negative / arr.Count);
+            Console.WriteLine(zero / arr.Count);
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            List<int> list = new();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Program.PlusMinus(list);
+        }
+    }
+}
