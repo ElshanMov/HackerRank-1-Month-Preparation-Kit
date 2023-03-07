f26fa7fa974a5ceac7efc5c4c462f36cf31cb756
ElshanMov <elshan.mov.772@gmail.com>
2023-03-07 13:07:04 -0800

    MinMaxSum

---
+++ MinMaxSum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MinMaxSum
+{
+    internal class Program
+    {
+        public static void miniMaxSum(List<int> arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+            int sum = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i]<min)
+                {
+                    min= arr[i];
+                }
+                if (arr[i]>max)
+                {
+                    max = arr[i];
+                }
+                sum += arr[i];
+            }
+
+            Console.WriteLine($"{sum-max} {sum-min}");
+        }
+        static void Main(string[] args)
+        {
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Program.miniMaxSum(arr);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Solved Algorithms
 
 [PlusMinus](https://www.hackerrank.com/challenges/one-month-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one)
+
+[MixMaxSum](https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one)
