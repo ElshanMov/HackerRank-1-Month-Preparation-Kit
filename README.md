fba9f02cf2d998d65cdee14cb143994b5d0ed75d
ElshanMov <elshan.mov.772@gmail.com>
2023-03-08 05:19:31 -0800

    Time Conversion Solved

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [PlusMinus](https://www.hackerrank.com/challenges/one-month-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one)
 
 [MixMaxSum](https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one)
+
+[Time Conversion](https://www.hackerrank.com/challenges/one-month-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one)
---
+++ TimeConversion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.IO;
+
+namespace TimeConversion
+{
+    internal class Program
+    {
+        //String Manipulation
+        public static string timeConversion(string s)
+        {
+            string process = s.Substring(8, 2);
+            s = s.Substring(0, 8);
+            int processPM= Convert.ToInt32(s.Substring(0, 2));
+            if (process=="AM")
+            {
+                if (s.Contains("12"))
+                {
+                    s = s.Replace("12", "00");
+                }
+            }
+            else
+            {
+                if (!s.Contains("12"))
+                {
+                    processPM += 12;
+                    s = s.Insert(0, processPM.ToString());
+                    s = s.Remove(2, 2);
+                }
+                
+            }
+            return s;
+        }
+
+        //Parse to DateTime
+        public static string timeConversion2(string s) {
+            // tt=> AM|PM      CultureInfo.InvariantCulture=>provide to globalization
+            var dt = DateTime.ParseExact(s, "hh:mm:sstt", CultureInfo.InvariantCulture);
+            return $"{dt:HH:mm:ss}";
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string result = Program.timeConversion(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
