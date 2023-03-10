0528fa70994ff5cb3b616586d60d2dfd8d292504
ElshanMov <elshan.mov.772@gmail.com>
2023-03-10 10:55:24 -0800

    LonelyInteger with LINQ

---
+++ Lonely Integer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Lonely_Integer
+{
+    internal class Program
+    {
+        public static int lonelyinteger(List<int> a)
+        {
+            
+            return 0;
+        }
+        //LINQ
+        public static int lonelyinteger1(List<int> a)
+        {
+            
+            return a.GroupBy(x => x).Where(a=>a.Count()<2).First().Key;
+            
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            int result = Program.lonelyinteger(a);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SparseArrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+
+namespace SparseArrays
+{
+    internal class Program
+    {
+        public static List<int> matchingStrings(List<string> strings, List<string> queries)
+        {
+            List<int> result = new();
+            int r;
+            for (int i = 0; i < queries.Count; i++)
+            {
+                r = 0;
+                for (int j = 0; j < strings.Count; j++)
+                {
+                    if (queries[i] == strings[j])
+                    {
+                        r += 1;
+                    }
+                }
+                result.Add(r);
+            }
+            return result;
+        }
+        //LINQ
+        public static List<int> matchingStrings2(List<string> strings, List<string> queries)
+        {
+            return queries.Select(query =>
+            {
+                 return strings.Where(str => query == str).Count();
+            }).ToList();
+        }
+        static void Main(string[] args)
+        {
+            int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> strings = new List<string>();
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                string stringsItem = Console.ReadLine();
+                strings.Add(stringsItem);
+            }
+
+            int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> queries = new List<string>();
+
+            for (int i = 0; i < queriesCount; i++)
+            {
+                string queriesItem = Console.ReadLine();
+                queries.Add(queriesItem);
+            }
+
+            List<int> res = Program.matchingStrings(strings, queries);
+
+            Console.WriteLine(String.Join("\n", res));
+        }
+    }
+}
